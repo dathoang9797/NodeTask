e035976a7e75964ea7cda02a3692875174718c97
Hoàng Quý Đạt <46701740+dathoang9797@users.noreply.github.com>
2022-06-26 00:34:24 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,9 @@
-import '@Configs/DB_Connect';
-import { routerUser } from '@Routers/UserRouter';
-import express from 'express';
+import http from 'http';
+import app from './app';
 
-const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
-//middleware: body-parser
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-//router
-app.use('/api/users',routerUser)
-
-app.listen(port, () => {
-  console.log('Server running on port: ' + port);
+server.listen(port, () => {
+  console.log('Server running on:' + port);
 });
