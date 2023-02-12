b9a3873318a725e87db1957fab37f397f936ac73
dat <dat.hoangquy@haravan.com>
2023-02-12 21:07:35 +0700

    Fix

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { routerUser } from '@Routers/UserRouter';
 import './Configs/DB_Connect';
 import './Models/TaskModels';
 import './Models/UserModels';
@@ -10,5 +11,6 @@ if (process.env.NODE_ENV === 'development')
     app.use(morgan('dev'))
 
 app.use(express.json());
+app.use(routerUser);
 
 export default app;
