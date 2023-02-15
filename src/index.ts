9ae5e4b6e99034b7ceabf80e30fa84f34c29b520
dat <dat.hoangquy@haravan.com>
2023-02-15 20:34:05 +0700

    up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import http from 'http';
 import app from './app';
 
-const server = http.createServer(app);
+export const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
