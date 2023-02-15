d3d71c9acc941b8167d3b100c9ed5a03ee58aaf3
dat <dat.hoangquy@haravan.com>
2023-02-15 20:33:26 +0700

    Fix

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import http from 'http';
-import app from './app';
-
-const server = http.createServer(app);
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log('Server running on:' + port);
-});
